29193ddb28c71af2c792eabbb8d7db3a3aaddac1
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-06-22 16:55:40 -0400

    master clone

---
+++ assets/javascript/main.js
@@ -0,0 +1,23 @@
+$(function (){
+   
+    var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
+    $.getJSON( marvelAPI, {
+        apikey: 'db60b2d90bd12165a42c7f7d1b0417ec'
+      })
+        .done(function( response ) {
+          var results = response.data.results;
+          var resultsLen = results.length;
+          var output = '<ul>'; 
+          
+          for(var i=0; i<resultsLen; i++){
+            if(results[i].images.length > 0) {
+              var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+              output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+            }
+          }  
+          output += '</ul>'
+          $('#results').append(output);
+    }).fail(function(err) {
+        throw err;
+    });
+})
