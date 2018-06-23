7f042be6ef1f48651fcd133b232314f8512db4de
Edward Nunez <edwardnnz@gmail.com>
2018-06-23 15:10:25 -0400

    removed node files, updated readme, small fixes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,11 @@
 
 $("form").submit(function (event) {
     event.preventDefault();
-    userInput = $("#user-input").val().trim();
+    userInput = $("#search").val().trim();
     console.log(userInput);
     superAPI.callAPI(userInput);
     var marvelResponse = marvelAPI.callAPI('characters',
                                          marvelAPI.params.characters);
-    console.log(marvelReponse);
+    console.log(marvelResponse);
 });
 
