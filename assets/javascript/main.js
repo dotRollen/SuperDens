857dc2a7e62765eb14f604e0ab480c17372c0311
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-06-23 12:59:30 -0400

    updated main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,7 @@
-marvelAPI.callAPI();
-superAPI.callAPI();
+$("form").submit(function (event) {
+    event.preventDefault();
+    userInput = $("#user-input").val().trim();
+    console.log(userInput);
+    superAPI.callAPI(userInput);
+    marvelAPI.callAPI(userInput);
+});
--- assets/javascript/super.js
+++ assets/javascript/super.js
@@ -1,20 +1,19 @@
 // CORS redirect to fix SuperHeroAPI CORS issue
-jQuery.ajaxPrefilter(function(options) {
-        if (options.crossDomain && jQuery.support.cors) {
-            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-        }
-    });
+jQuery.ajaxPrefilter(function (options)  {
+    if  (options.crossDomain  &&  jQuery.support.cors)  {
+        options.url  =  'https://cors-anywhere.herokuapp.com/'  +  options.url;
+    }
+});
 
 const superAPI = {
-    callAPI: function () {
+    callAPI: function (superInput) {
         // function for superheroapi
-        var heroSearch = "Batman"; //just an exampe search
-        var superAPI = 'http://superheroapi.com/api/10160533766455290/search/' + heroSearch  + '/';
-        $.getJSON(superAPI).then(function(response) {
+        var superAPI = 'http://superheroapi.com/api/10160533766455290/search/' + superInput + '/';
+        $.getJSON(superAPI).then(function (response) {
             console.log(response);
         });
     },
 }
 
 
-    
+
